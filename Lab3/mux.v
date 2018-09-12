aae0b17d6d924d5dc77beb5b75605c721ec39eb7
Sukmin Hwang <SukMin@Sukmins-MacBook-Pro.local>
2018-09-12 01:54:55 -0500

    file submission

--- Lab3/mux.v
+++ Lab3/mux.v
@@ -4,12 +4,13 @@ module mux2(out, A, B, control);
     input  A, B;
     input  control;
     wire   wA, wB, not_control;
-         
+
     not n1(not_control, control);
     and a1(wA, A, not_control);
     and a2(wB, B, control);
     or  o1(out, wA, wB);
 endmodule // mux2
+
 
 // output is A (when control == 00) or B (when control == 01) or
 //           C (when control == 10) or D (when control == 11)
@@ -17,5 +18,11 @@ module mux4(out, A, B, C, D, control);
     output      out;
     input       A, B, C, D;
     input [1:0] control;
+    wire w1, w2;
+
+    mux2 mx1 (w1, A,B,control[0]);
+    mux2 mx2 (w2, A,B,control[0]);
+
+    mux2 mx3 (out, w1,w2,control[1]);
 
 endmodule // mux4
