a03fd409c615bd548a19eb9510d11a52c8ba2333
Suleman <msm6@illinois.edu>
2018-09-17 11:33:31 -0500

    Lab4 release

---
+++ Lab4/palindrome_control.v
@@ -0,0 +1,8 @@
+
+module palindrome_control(palindrome, done, select, load, go, a_ne_b, front_ge_back, clock, reset);
+	output load, select, palindrome, done;
+	input go, a_ne_b, front_ge_back;
+	input clock, reset;
+
+	
+endmodule // palindrome_control 
