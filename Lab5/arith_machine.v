e98f2c323f79089126f8bd0ab965dd6a5c5013e8
Sukmin Hwang <SukMin@Sukmins-MacBook-Pro.local>
2018-09-27 16:46:13 -0500

    file submission

--- Lab5/arith_machine.v
+++ Lab5/arith_machine.v
@@ -8,21 +8,33 @@ module arith_machine(except, clock, reset);
     output      except;
     input       clock, reset;
 
-    wire [31:0] inst;  
-    wire [31:0] PC;  
+    wire [31:0] inst, PC, nextPC, rsData, rtData, rdData, B, imm32;
+    wire [4:0] rdNum;
+    wire [2:0] alu_op;
+    wire rd_src, alu_src2, rdWriteEnable;
+
+    assign imm32[31:0] =  { {16{inst[15]}}, {inst[15:0]} }; //sign extender
+
+
+    //wire [31:0] PC;
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    register #(32) PC_reg( /* connect signals */ );
+    register #(32) PC_reg(PC, nextPC, clock, 1, reset);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+    instruction_memory im(inst, PC[31:2]);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    regfile rf ( /* connect signals */ );
+    regfile rf (rsData, rtData, inst[25:21], inst[20:16], rdNum, rdData, rdWriteEnable, clock, reset);
 
     /* add other modules */
-   
+    mux2v mx1(rdNum, inst[15:11], inst[20:16], rd_src);
+    mux2v mx2(B, rtData, imm32, alu_src2);
+    mips_decode md1(alu_op, rdWriteEnable, rd_src, alu_src2, except, inst[31:26], inst[5:0]);
+    alu32 al1(rdData, , , ,rsData, B, alu_op);
+    alu32 al2(nextPC, , , ,PC, 32'b100, 3'b010);
+
 endmodule // arith_machine
