8ae3bfc1173bb83a0f885c40c1b885155d553c19
Sukmin Hwang <SukMin@wirelessprv-10-192-242-221.near.illinois.edu>
2018-10-11 15:25:49 -0500

    file submission

--- Lab7/p1.s
+++ Lab7/p1.s
@@ -1,7 +1,7 @@
 .text
 
 ## bool has_single_bit_set(unsigned value) {  // returns 1 if a single bit is set
-##   if (value == 0) {  
+##   if (value == 0) {
 ##     return 0;   // has no bits set
 ##   }
 ##   if (value & (value - 1)) {
@@ -11,8 +11,23 @@
 ## }
 
 .globl has_single_bit_set
+
 has_single_bit_set:
-	jr	$ra
+	bne		$a0,	 0,	 next			# if value !=0, go to next.
+	li 		$v0,	 0						# if value == 0, return 0.
+	jr 		$ra									# Exit
+
+next:
+	sub	 	$t1, $a0, 	1						# t1 = value -1
+	and	  $t2, $a0, 	$t1					# t2 = value & (value-1)
+
+	beq	  $t2, 	 0, 	final				# if(value & value-1 == 0), go to final
+	li		$v0,	 0								# return 0
+	jr		$ra											# Exit
+
+final:
+	li 		$v0,		1						 # return 1.
+	jr		$ra									 # Exit
 
 
 ## unsigned get_lowest_set_bit(unsigned value) {
@@ -25,5 +40,22 @@ has_single_bit_set:
 ## }
 
 .globl get_lowest_set_bit
+
 get_lowest_set_bit:
-	jr	$ra
+	li		$t0, 			0 			   	#i = 0
+
+loop:
+	li 		   	$t3,	 	 		1								  	# store $t3 = 1.
+	sllv		  $t2, 	    	$t3,			$t0				# $t2 = (1 << t)
+	and		    $t1, 	    	$a0, 			$t2				# $t1 = value & (1 << i)
+	bne 		  $t1, 		  	0,		   	final2		# if $t1 != 0, go to final2 and return i.
+
+	add 	  	$t0,		   $t0,		    1			  	# i++
+	blt 		  $t0,			 16, 	  		loop		  # i<16
+
+	li 		  	$v0, 				0									  # if nothing was returned, return 0/
+	jr		  	$ra													  	# Exit
+
+final2:
+	move 	  	$v0,			$t0									  # return 0.
+	jr		    $ra								     				  # Exit
