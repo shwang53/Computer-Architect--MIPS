9445489ca76b99cac71e54c2c1c513762e994ad1
Sukmin Hwang <SukMin@wirelessprv-10-192-242-221.near.illinois.edu>
2018-10-19 20:30:48 -0500

    file submission

--- Lab8/p2.s
+++ Lab8/p2.s
@@ -6,6 +6,7 @@
 ##   for (int i = 0 ; i < GRID_SQUARED ; ++ i) {
 ##     for (int j = 0 ; j < GRID_SQUARED ; ++ j) {
 ##       unsigned value = board[i][j];
+
 ##       if (has_single_bit_set(value)) {
 ##         for (int k = 0 ; k < GRID_SQUARED ; ++ k) {
 ##           // eliminate from row
@@ -14,7 +15,7 @@
 ##               board[i][k] &= ~value;
 ##               changed = true;
 ##             }
-##           }
+##           } next1
 ##           // eliminate from column
 ##           if (k != i) {
 ##             if (board[k][j] & value) {
@@ -39,6 +40,7 @@
 ##           }
 ##         }
 ##       }
+
 ##     }
 ##   }
 ##   return changed;
@@ -46,7 +48,7 @@
 
 .globl rule1
 rule1:
-			sub 	$sp,  $sp, 	16				#allocating 16bytes on stack.
+			sub 	$sp,  $sp, 	28				#allocating 20bytes on stack.
 
 			sw		$ra, 	0($sp)					# allocate return in stack.
 
@@ -59,22 +61,135 @@ rule1:
 			sw		$s2, 	12($sp)					# creating safe register in stack
 			move	$s2,  $a0					  	# allocate  (argument) in safe register
 
+			sw		$s3, 	16($sp)					# creating safe register in stack
+			li		$s3,  0				  			# allocate  (bool changed) in safe register
+
+			sw 		$s4,	20($sp)
+			sw		$s5,	24($sp)					#t1 stored
+
 rule1_loop:
-			mul		$t2,	$s0,	16				# N(size) * i
-			add 	$t3,	$t2,	$s1				# N*i +j
-			mul		$t4,	$t3,	2					# (N*i +j) * sizeOfElement(2,short)
-			add 	$t4,	$s2,	$t4				# &board[i][j]
-			lhu		$a0,	0($t4)					# at $a0, dereference board [i][j]
+			mul		$t0,	$s0,	16				# N(size) * i
+			add 	$t0,	$t0,	$s1				# N*i +j
+			mul		$t0,	$t0,	2					# (N*i +j) * sizeOfElement(2,short)
+			add 	$t0,	$s2,	$t0				# &board[i][j]
+			lhu		$a0,	0($t0)					# at $a0(value), dereference board [i][j]
+			move 	$s4,	$a0 						 # s4 = value
 
 			jal		has_single_bit_set		# jump and link to the function.
-			beq 	$v0,	0,		skip 			# if(bool) is false -> go to skip and return false.
 
+			beq 	$v0,	0,	skip_in_loop 			# if(bool) is false -> go to skip_in_loop
+
+			li		$t1,	0								# k = 0;
+			move 	$s5,	$t1
+
+first_loop:
+			## for loop again ##
+			beq 	$s5,	$s1,	next1				#k == j ? go next1
+
+			mul		$t2,	$s0,	16					# N(size) * i
+			add 	$t2,	$t2,	$s5					# N*i + k(t1)
+			mul		$t2,	$t2,	2						# (N*i +k) * sizeOfElement(2,short)
+			add 	$t2, 	$s2,	$t2					# &board[i][k]
+			lhu		$t3,	0($t2)						# t3 = board[i][k]
+
+#if (board[i][k] & value)
+			and 	$t4,	$s4, 	$t3					# t4 = board[i][k] & value
+			beq		$t4,	0,	next1					# if false, next1
+
+			not 	$t5,	$s4								# ~value
+			and 	$t3,	$t3,	$t5					# board[i][k] &= ~value
+			li		$s3,		1								# changed = true;
+
+next1:
+			beq 	$s5,	$s0,	next2				#k == i ? go next2
+
+			mul		$t6,	$s5,	16					#  N(size) * k
+			add 	$t6,	$t6,	$s1					#  N*k +j
+			mul		$t6,	$t6,	2						# (N*k +j) * sizeOfElement(2,short)
+			add 	$t6,	$s2,	$t6					# &board[k][j]
+			lhu		$t7,	0($t6)						# at $t7, dereference board [k][j]
+
+#if (board[k][j] & value)
+			and 	$t4,	$s4,	$t7
+			beq		$t4,	0,	next2
+
+			not 	$t5,	$s4								# ~value
+			and 	$t4,	$t5,	$t4					# board[i][k] &= ~value
+			li		$s3,		1								# changed = true;
+
+next2:
+			add 	$s5,	$s5,	1						# k++;
+			blt 	$s5,	16,	first_loop		# if k<4, come back to first_loop
+			#li		$s5,	0									# k = 0 reset.
+
+
+body1:
+			move	$a0,	$s0
+			jal		get_square_begin
+			move	$t8, $v0								# int ii = get_square_begin(i);
+
+			move	$a0,	$s1
+			jal		get_square_begin
+			move	$t9, $v0								# int jj = get_square_begin(j);
+
+			move		$t1,	$t8							# k = ii
+			move		$t2,	$t9							# l = jj
+
+			add 		$t8, 	$t8,	4
+			add 		$t9,	$t9,	4
+
+second_loop:
+
+
+			mul		$t3,	$t1,	16				  # N(size) * k
+			add 	$t3,	$t3,	$t2				# N*k +l
+			mul		$t3,	$t3,	2					# (N*k +l) * sizeOfElement(2,short)
+			add 	$t3,	$s2,	$t3				# &board[k][l]
+			lhu		$t6,	0($t3)					# at $a0(value), dereference board [i][j]
+
+			bne  	$t1,	$s0,	next3			#two conditional
+			bne 	$t2,	$s1, 	next3
+			j			next4
+
+next3:
+
+			and 	$t6,	$s4,	$t6
+			beq		$t6,	0,	next4
+
+			not 	$t5,	$s4									# ~value
+			and 	$t6,	$t5,	$t6						# board[i][k] &= ~value
+			li		$s3,		1									# changed = true;
+
+
+next4:
+			add 	$t2,	$t2,	1							# l++;
+			blt 	$t2,	$t9, 	second_loop		# if l<4, come back to loop.
+			li 		$t2,		0									# reset j = 0
+
+			add 	$t1,	$t1,	1							# k++;
+			blt 	$t1,	$t8, 	second_loop		# if k<4, come back to loop.
+
+
+
+skip_in_loop:
 
 			add 	$s1,	$s1,	1					# j++;
-			blt 	$s1,	16, 	p2_loop		# if j<16, come back to loop.
+			blt 	$s1,	16, 	rule1_loop		# if j<16, come back to loop.
 			li 		$s1,		0							# reset j = 0
 
 			add 	$s0,	$s0,	1					# i++;
-			blt 	$s0,	16, 	p2_loop		# if i<16, come back to loop.
+			blt 	$s0,	16, 	rule1_loop		# if i<16, come back to loop.
 
-	jr	$ra
+
+finally:
+			move 	$v0,	$s3							# return v0 = changed.
+
+			lw		$ra,	0($sp)					# load return value again.
+			lw 		$s0, 	4($sp)					# load s0 to i again
+			lw 		$s1, 	8($sp)					# load s1 to j again
+			lw		$s2, 	12($sp)					# load s2 to (argument) again
+			lw		$s3, 	16($sp)					# load s3 to (changed) again
+			lw		$s4,	20($sp)
+			lw		$s5,	24($sp)
+			add 	$sp,  $sp, 	28				# clear the memory.
+			jr		$ra										# Exit
--- Lab8/p2_helpers.s
+++ Lab8/p2_helpers.s
@@ -10,17 +10,130 @@ get_square_begin:
 # (feel free to copy over the solution afterwards)
 .globl has_single_bit_set
 has_single_bit_set:
-	jr	$ra
+	bne		$a0,	 0,	 next			# if value !=0, go to next.
+	li 		$v0,	 0						# if value == 0, return 0.
+	jr 		$ra									# Exit
+
+next:
+	sub	 	$t1, $a0, 	1						# t1 = value -1
+	and	  $t2, $a0, 	$t1					# t2 = value & (value-1)
+
+	beq	  $t2, 	 0, 	final				# if(value & value-1 == 0), go to final
+	li		$v0,	 0								# return 0
+	jr		$ra											# Exit
+
+final:
+	li 		$v0,		1						 # return 1.
+	jr		$ra									 # Exit
+
 
 
 # UNTIL THE SOLUTIONS ARE RELEASED, YOU SHOULD COPY OVER YOUR VERSION FROM LAB 7
 # (feel free to copy over the solution afterwards)
 get_lowest_set_bit:
-	jr	$ra
+	li		$t0, 			0 			   	#i = 0
+
+loop:
+	li 		   	$t3,	 	 		1								  	# store $t3 = 1.
+	sllv		  $t2, 	    	$t3,			$t0				# $t2 = (1 << t)
+	and		    $t1, 	    	$a0, 			$t2				# $t1 = value & (1 << i)
+	bne 		  $t1, 		  	0,		   	final2		# if $t1 != 0, go to final2 and return i.
+
+	add 	  	$t0,		   $t0,		    1			  	# i++
+	blt 		  $t0,			 16, 	  		loop		  # i<16
+
+	li 		  	$v0, 				0									  # if nothing was returned, return 0/
+	jr		  	$ra													  	# Exit
+
+final2:
+	move 	  	$v0,			$t0									  # return i.
+	jr		    $ra								     				  # Exit
 
 
 # UNTIL THE SOLUTIONS ARE RELEASED, YOU SHOULD COPY OVER YOUR VERSION FROM LAB 7
 # (feel free to copy over the solution afterwards)
 .globl print_board
 print_board:
-	jr	$ra
+				sub 	$sp,  $sp, 	16				#allocating 16bytes on stack.
+
+				sw		$ra, 	0($sp)					# allocate return in stack.
+
+				sw		$s0, 	4($sp)					# creating safe register in stack
+				li		$s0,  0								# allocate (i) in safe register.
+
+				sw		$s1, 	8($sp)					# creating safe register in stack
+				li		$s1,  0								# allocate (j) in safe register.
+
+				sw		$s2, 	12($sp)					# creating safe register in stack
+				move	$s2,  $a0					  	# allocate  (argument) in safe register
+
+
+
+p22_loop :
+				mul		$t2,	$s0,	16				# N(size) * i
+				add 	$t3,	$t2,	$s1				# N*i +j
+				mul		$t4,	$t3,	2					# (N*i +j) * sizeOfElement(2,short)
+				add 	$t4,	$s2,	$t4				# &board[i][j]
+
+				lhu		$a0,	0($t4)					# at $a0, value = board [i][j]
+
+				jal		has_single_bit_set		# jump and link to the function.
+				bne 	$v0,	0,	 p22_skip 	# if(bool) is true -> go to skip
+
+				#putchar(c)
+				li		$a0,	'*'
+				li		$v0,  11
+				syscall
+
+dine_printing_char:
+				add 	$s1,	$s1,	1				  	# j++;
+				blt 	$s1,	16, 	p22_loop  	# if j<16, come back to loop.
+
+				#putchar('\n')
+				li		$a0,	'\n'
+				li		$v0,   11
+				syscall
+
+				li 		$s1,		0						  	# reset j = 0
+
+				add 	$s0,	$s0,	1				    # i++;
+				blt 	$s0,	16, 	p22_loop		# if i<16, come back to loop.
+
+
+				lw		$ra,	0($sp)					# load return value again.
+				lw 		$s0, 	4($sp)					# load s0 to i again
+				lw 		$s1, 	8($sp)					# load s1 to j again
+				lw		$s2, 	12($sp)					# load s2 to argument again
+				add 	$sp,  $sp, 	16				# clear the memory.
+				jr		$ra										# Exit
+
+
+p22_skip:
+
+				mul		$t2,	$s0,	16				# N(size) * i
+				add 	$t3,	$t2,	$s1				# N*i +j
+				mul		$t4,	$t3,	2					# (N*i +j) * sizeOfElement(2,short)
+				add 	$t4,	$s2,	$t4				# &board[i][j]
+				lhu		$a0,	0($t4)					# at $a0, value = board [i][j]
+
+				#int num = get_lowest_set_bit(value) + 1;
+				jal		get_lowest_set_bit
+				add   $t5,	$v0,	1
+
+				# c = symbollist[num];
+				la 		$t6, symbollist
+				add 	$t6,	$t5,	$t6				#
+				lbu		$a0,	0($t6)					#
+				li		$v0,   11
+				syscall
+
+				j			dine_printing_char
+
+p22_note:
+
+				lw		$ra,	0($sp)					# load return value again.
+				lw 		$s0, 	4($sp)					# load s0 to i again
+				lw 		$s1, 	8($sp)					# load s1 to j again
+				lw		$s2, 	12($sp)					# load s2 to argument again
+				add 	$sp,  $sp, 	16				# clear the memory.
+				jr		$ra										# Exit
