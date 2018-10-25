1586f47b37ad60938f8691e6f8a2ca727dc9b9a2
Sukmin Hwang <SukMin@wirelessprv-10-192-242-221.near.illinois.edu>
2018-10-25 15:23:53 -0500

    file submission

--- Lab9/timer.v
+++ Lab9/timer.v
@@ -11,4 +11,24 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     // HINT: make your interrupt cycle register reset to 32'hffffffff
     //       (using the reset_value parameter)
     //       to prevent an interrupt being raised the very first cycle
+
+    wire  timeRead, timeWrite, acknowledge;
+    wire [31:0] qCycle, dCycle, qInterrupt;
+
+    register #(, 32'hffffffff) interrupt_cycle(qInterrupt, data, clock, timeWrite, reset);
+
+    register cycle_counter(qCycle, dCycle, clock, 1'd1, reset);
+
+    register #(1, ) interupt_line(TimerInterrupt, 1'd1, clock, (qCycle == qInterrupt), reset | acknowledge);
+
+    alu32 alu1(dCycle, , , `ALU_ADD, qCycle, 32'd1);
+
+    tristate trid(cycle, qCycle, timeRead);
+
+    assign timeRead = (32'hffff001c == address) & MemRead;
+    assign timeWrite = (32'hffff001c == address) & MemWrite;
+    assign acknowledge = (32'hffff006c == address) & MemWrite;
+    assign TimerAddress = (32'hffff001c == address) | (32'hffff006c == address);
+
+
 endmodule
