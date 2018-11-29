54bd2da5517cc07b12221bb8583743657c853d2a
Sukmin Hwang <SukMin@wirelessprv-10-195-119-9.near.illinois.edu>
2018-11-29 14:19:37 -0600

    file submission

--- Lab12/utils.cpp
+++ Lab12/utils.cpp
@@ -23,17 +23,9 @@ uint32_t extract_index(uint32_t address, const CacheConfig& cache_config) {
 uint32_t extract_block_offset(uint32_t address, const CacheConfig& cache_config) {
   // TODO
 
-	// if (cache_config.get_num_block_offset_bits()){
-  //   return address << (32 - cache_config.get_num_block_offset_bits()) >> (32 - cache_config.get_num_block_offset_bits());
-  // } else{
-  //   return 0;
-  // }
-  // uint32_t index_bits = cache_config.get_num_index_bits();
-  //   uint32_t tag_bits = cache_config.get_num_tag_bits();
-
-    if(cache_config.get_num_tag_bits() > 31) return 0;
-
-  //  uint32_t tag_ind = tag_bits + index_bits;
-
-     return ( (address << (cache_config.get_num_index_bits() +cache_config.get_num_tag_bits()) ) >> (cache_config.get_num_index_bits()+ cache_config.get_num_tag_bits()));
+    if(cache_config.get_num_tag_bits() <= 31){
+      return ( (address << (cache_config.get_num_index_bits() +cache_config.get_num_tag_bits()) ) >> (cache_config.get_num_index_bits()+ cache_config.get_num_tag_bits()));
+    } else{
+      return 0;
+    }
 }
--- Lab13/transpose.cpp
+++ Lab13/transpose.cpp
@@ -11,9 +11,23 @@ using std::min;
 // modify this function to add tiling
 void
 transpose_tiled(int **src, int **dest) {
-    for (int i = 0; i < SIZE; i ++) {
-        for (int j = 0; j < SIZE; j ++) {
-            dest[i][j] = src[j][i];
+
+  int adder = 32;
+
+    for (int i = 0; i < SIZE; i += adder) {
+        for (int j = 0; j < SIZE; j += adder) {
+
+          int minX = min(i+adder,SIZE);
+          int minY = min(j+adder,SIZE);
+
+            for(int x=i; x< minX; x++){
+              for(int y=j; y<minY; y++){
+
+                  dest[x][y] = src[y][x];
+
+              }
+            }
+
         }
     }
 }
