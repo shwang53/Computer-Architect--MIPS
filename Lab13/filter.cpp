92922ab68b4d1eda4d120593eabad7996651aafe
Sukmin Hwang <SukMin@Sukmins-MacBook-Pro.local>
2018-12-02 17:43:47 -0600

    file submission

--- Lab13/filter.cpp
+++ Lab13/filter.cpp
@@ -19,12 +19,7 @@ filter_fusion(pixel_t **image1, pixel_t **image2) {
       }
     }
 
-    //
-    // for (int i = 1; i < SIZE; i ++) {
-    //     if (i < SIZE - 1){ filter1(image1, image2, i); }
-    //     if (i >= 2 && i < SIZE - 2){ filter2(image1, image2, i); }
-    //     if (i >= 6){ filter3(image2, i - 5); }
-    // }
+\\
 }
 
 // modify this code by adding software prefetching
@@ -53,20 +48,23 @@ filter_prefetch(pixel_t **image1, pixel_t **image2) {
 void
 filter_all(pixel_t **image1, pixel_t **image2) {
     for (int i = 1; i < SIZE - 1; i ++) {
-        filter1(image1, image2, i);
         __builtin_prefetch(image1[i + 10], 0, 3);
-         __builtin_prefetch(image2[i + 10], 1, 3);
+        __builtin_prefetch(image2[i + 10], 1, 3);
+        filter1(image1, image2, i);
+
     }
 
     for (int i = 2; i < SIZE - 2; i ++) {
-        filter2(image1, image2, i);
         __builtin_prefetch(image1[i + 10], 0, 3);
-           __builtin_prefetch(image2[i + 10], 1, 3);
+        __builtin_prefetch(image2[i + 10], 1, 3);
+        filter2(image1, image2, i);
+
     }
 
     for (int i = 6; i < SIZE; i ++) {
-        filter3(image2, i-5);
         __builtin_prefetch(image1[i + 10], 0, 3);
         __builtin_prefetch(image2[i + 10], 1, 3);
+        filter3(image2, i-5);
+
     }
 }
